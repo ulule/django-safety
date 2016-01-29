23c020cd1f37f3ec5c082f887ae6af19d33ab081
Gilles Fabio <gilles@gillesfabio.com>
2016-01-29 14:59:36 +0100

    Fix tests.

--- safety/tests/test_views.py
+++ safety/tests/test_views.py
@@ -18,6 +18,6 @@ class ViewsTest(BaseTestCase):
         self.assertEqual(obj.user, self.user)
         self.assertTrue(obj.session_key)
         self.assertEqual(obj.ip, '127.0.0.1')
-        self.assertEqual(obj.device, 'Safari 6.0.2 - Mac OS X 10.2.8')
+        self.assertEqual(obj.device, 'Safari 6.0.2 - Mac OS X 10.8.2')
 
         self.client.logout()
