51f921424a33ae122d19e108ee7d789e10f6a62b
Gilles Fabio <gilles@gillesfabio.com>
2016-01-29 14:21:08 +0100

    Fix tests.

--- safety/tests/test_models.py
+++ safety/tests/test_models.py
@@ -7,5 +7,6 @@ from safety.models import Session
 class ModelsTest(BaseTestCase):
     def test_create_session(self):
         self.assertEqual(Session.objects.count(), 0)
-        self.client.login(username=self.user.username, password='secret')
+        self.login_user()
         self.assertEqual(Session.objects.count(), 1)
+        self.client.logout()
